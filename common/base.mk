419e72560e7193cca404b804487b8cd5f359f702
Dan Pasanen <dan.pasanen@gmail.com>
2017-08-22 11:11:09 -0500

    marlin: don't build mm-qcamera-app
    * Its unecessary (test app) and broken
    
    Change-Id: I4784519de822ff55d542dedbeb487efa53beb731
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- common/base.mk
+++ common/base.mk
@@ -331,7 +331,6 @@ LIBCAMERA += libmmcamera_interface2
 LIBCAMERA += libmmjpeg_interface
 LIBCAMERA += libmmlib2d_interface
 LIBCAMERA += libqomx_core
-LIBCAMERA += mm-qcamera-app
 LIBCAMERA += camera_test
 LIBCAMERA += org.codeaurora.camera
 LIBCAMERA += libgooglecamerahal
