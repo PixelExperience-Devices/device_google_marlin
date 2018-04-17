27a7717d94040d748de328857d6265abf128efd1
razorloves <razorloves@gmail.com>
2018-04-16 19:52:00 -0500

    marlin: Add IMS symlinks
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- Android.mk
+++ Android.mk
@@ -27,6 +27,16 @@ ifneq ($(filter marlin sailfish, $(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
+IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT)/app/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+$(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "IMS lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/lib64/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
