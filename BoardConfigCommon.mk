78ab78bb669c24e147226f938c6b3f5c54ccb276
Lunarixus <Nathanb644@hotmail.co.uk>
2020-09-11 09:57:03 +0100

    marlin: Use LunarKernel
    Signed-off-by: Lunarixus <Nathanb644@hotmail.co.uk>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -125,7 +125,7 @@ endif
 
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_CONFIG := marlin_defconfig
+TARGET_KERNEL_CONFIG := LunarKernel_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_KERNEL_SOURCE := kernel/google/marlin
 TARGET_COMPILE_WITH_MSM_KERNEL := true
