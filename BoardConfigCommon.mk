e71ba111727d16285f067f607d974049fb3cbad6
spezi77 <spezi7713@gmx.net>
2021-03-14 17:36:10 +0100

    marlin: switch to NeutrinoKernel_defconfig
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -124,7 +124,7 @@ endif
 
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_CONFIG := LunarKernel_defconfig
+TARGET_KERNEL_CONFIG := NeutrinoKernel_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_KERNEL_SOURCE := kernel/google/marlin
 TARGET_KERNEL_CLANG_COMPILE := true
