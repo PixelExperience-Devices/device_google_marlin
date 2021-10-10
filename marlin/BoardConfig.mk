decda5a07405edb4819da74f4137948ef04f86f2
PixelBoot <bootingpixels@gmail.com>
2021-10-10 13:29:22 -0700

    marlin: Remove AUX support

--- marlin/BoardConfig.mk
+++ marlin/BoardConfig.mk
@@ -1,7 +1,7 @@
 TARGET_BOOTLOADER_BOARD_NAME := marlin
 TARGET_BOARD_INFO_FILE := device/google/marlin/marlin/board-info.txt
 
-TARGET_AUX_OS_VARIANT_LIST := marlin
+#TARGET_AUX_OS_VARIANT_LIST := marlin
 
 BOARD_KERNEL_CMDLINE += androidboot.hardware=marlin
 
